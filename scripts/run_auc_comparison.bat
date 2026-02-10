3dccff494336afceed2dd6c3cfd517d6234771e4
LilianHeGao <lilianhegao@gmail.com>
2026-02-09 16:18:03 -0800

    batch auc

--- scripts/run_auc_comparison.bat
+++ scripts/run_auc_comparison.bat
@@ -8,8 +8,8 @@ set CACHE_ROOT=\\192.168.0.73\Papaya13\Lilian\merfish_sqi_cache
 set OUT_ROOT=output\auc_comparison
 
 set COND3=mouse_6OHDA:\\192.168.0.73\Papaya13\Sasha\20251105_6OHDA\H1\H1_PTBP1_TH_GFAP_set11
-set COND2=human_control:\\192.168.0.168\durian3\Lilian\022425_FTD_smFISH_MBP_NRGN\coverslip1_controls\MBP_NRGN_set5
-set COND1=human_FTD:\\192.168.0.168\durian3\Lilian\022425_FTD_smFISH_MBP_NRGN\coverslip2_FTD_group1\MBP_NRGN_set2
+set COND2=human_control:\\192.168.0.116\durian3\Lilian\022425_FTD_smFISH_MBP_NRGN\coverslip1_controls\MBP_NRGN_set5
+set COND1=human_FTD:\\192.168.0.116\durian3\Lilian\022425_FTD_smFISH_MBP_NRGN\coverslip2_FTD_group1\MBP_NRGN_set2
 
 python scripts\run_auc_comparison.py ^
     --conditions "%COND1%" "%COND2%" "%COND3%" ^
